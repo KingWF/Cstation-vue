b575af6094d89b570a9a4d0f8c898f7499780806
ZSCaiGou <11460269+upontime@user.noreply.gitee.com>
2024-07-11 10:35:57 +0800

    up菜单优化

--- src/views/up/UpPersonal.vue
+++ src/views/up/UpPersonal.vue
@@ -1,71 +1,104 @@
 <template>
   <div>
     <el-container>
-      <el-header style="padding: 0 ;">
-        <Header :hideLogo="hideLogo" :topColor="topColor" :keepSearchKey="searchKey" :enableSearch="enableSearch"></Header>
+      <el-header style="padding: 0">
+        <Header
+          :hideLogo="hideLogo"
+          :topColor="topColor"
+          :keepSearchKey="searchKey"
+          :enableSearch="enableSearch"
+        ></Header>
       </el-header>
-      <el-container class="up-container" >
-        <el-aside width="200px" style="height: 600px;">
+      <el-container class="up-container">
+        <el-aside width="200px" style="height: 600px">
           <!-- 菜单 -->
           <el-scrollbar>
-            <el-menu v-model="menuActiveIndex">
-              <el-menu-item index="1" @click="$router.push('/upPersonal/videoManage')">
-                <el-icon><VideoCamera/></el-icon>
-                视频管理</el-menu-item>
-              <el-menu-item index="2" @click="$router.push('/upPersonal/message')">
-                <el-icon><Message/></el-icon>
-                我的消息</el-menu-item>
+            <el-menu :default-active="menuActiveIndex">
+              <el-menu-item
+                index="1"
+                @click="$router.push('/upPersonal/videoManage')"
+              >
+                <el-icon><VideoCamera /></el-icon>
+                视频管理</el-menu-item
+              >
+              <el-menu-item
+                index="2"
+                @click="$router.push('/upPersonal/message')"
+              >
+                <el-icon><Message /></el-icon>
+                我的消息</el-menu-item
+              >
               <el-menu-item index="3">
-                <el-icon><Star/></el-icon>
-                我的收藏</el-menu-item>
+                <el-icon><Star /></el-icon>
+                我的收藏</el-menu-item
+              >
               <el-menu-item index="4">
-                <el-icon><Avatar/></el-icon>
-                我的粉丝</el-menu-item>
-              <el-menu-item index="5" @click="$router.push('/upPersonal/myMessage')">
-                <el-icon><Setting/></el-icon>
-                个人信息</el-menu-item>
+                <el-icon><Avatar /></el-icon>
+                我的粉丝</el-menu-item
+              >
+              <el-menu-item
+                index="5"
+                @click="$router.push('/upPersonal/myMessage')"
+              >
+                <el-icon><Setting /></el-icon>
+                个人信息</el-menu-item
+              >
             </el-menu>
           </el-scrollbar>
-
         </el-aside>
         <el-main style="">
           <!-- 二级路由view：切换页面中的部分内容（独立的页面） -->
-          <RouterView/>
+          <RouterView />
         </el-main>
       </el-container>
     </el-container>
   </div>
 </template>
 <script>
-import Header from '../../components/Header.vue'
-import { RouterLink, RouterView } from 'vue-router'
-export default{
-  components:{
-    Header
+import Header from "../../components/Header.vue";
+import { RouterLink, RouterView } from "vue-router";
+export default {
+  components: {
+    Header,
   },
-  methods:{
-    testFn(){
-      console.log('testFn')
-    }
+  methods: {
+    checkRoute() {
+      const path = this.$route.path;
+
+      if (path.includes("videoManage")) {
+        this.menuActiveIndex = "1";
+      }
+      if (path.includes("editVideo")) {
+        this.menuActiveIndex = "1";
+      }
+      if (path.includes("viewVideo")) {
+        this.menuActiveIndex = "1";
+      }
+      if (path.includes("message")) {
+        this.menuActiveIndex = "2";
+      }
+      if (path.includes("myMessage")) {
+        this.menuActiveIndex = "5";
+      }
+    },
   },
   mounted() {
-    
+    this.checkRoute();
   },
-  data(){
-    return{
-      menuActiveIndex: "1",
+  data() {
+    return {
+      menuActiveIndex: "0",
       hideLogo: false,
       hideSearch: false,
-      topColor: '#0e6ec9',
-      enableSearch:true
-    }
-  }
-
-}
+      topColor: "#0e6ec9",
+      enableSearch: true,
+    };
+  },
+};
 </script>
 <style scoped>
 /*  */
-.up-container{
+.up-container {
   margin-top: 50px;
 }
 </style>
